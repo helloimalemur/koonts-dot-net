2a892e15f62792dfbf0dfed1089fca304f22c7ca
foxx <jkoontsiii@gmail.com>
2022-08-23 18:13:44 -0400

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from "./customer/customer.component";
+import { TicketComponent } from "./ticket/ticket.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'customer-component', component: CustomerComponent },
+  { path: 'ticket-component', component: TicketComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
