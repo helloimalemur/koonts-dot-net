edaad0974a605e60a4afca533aec04a8ae15b83b
foxx <jkoontsiii@gmail.com>
2022-08-23 22:52:57 -0400

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'koonts'`, () => {
+  it(`should have as title 'Koonts Consulting'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('koonts');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('koonts app is running!');
+    expect(app.title).toEqual('Koonts Consulting');
   });
 });
