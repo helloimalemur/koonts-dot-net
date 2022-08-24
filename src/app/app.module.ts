fdf01266293ec19f6bfb3345ce9aebed1dbea1d1
foxx <jkoontsiii@gmail.com>
2022-08-23 23:15:59 -0400

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import {RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomerComponent } from './customer/customer.component';
 import { TicketComponent } from './ticket/ticket.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConsultingComponent } from './consulting/consulting.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
 
 const routes: Routes = [
   { path: 'customer-component', component: CustomerComponent },
   { path: 'ticket-component', component: TicketComponent },
+  { path: 'consulting-component', component: ConsultingComponent},
+  { path: 'portfolio-component', component: PortfolioComponent },
+  { path: 'contact-component', component: ContactComponent },
+  { path: 'about-component', component: AboutComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
-    TicketComponent
+    TicketComponent,
+    ConsultingComponent,
+    PortfolioComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
