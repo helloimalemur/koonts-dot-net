5e81383a00c84177b986004a657e7f4be07e8f74
K <37273704+helloimalemur@users.noreply.github.com>
2022-08-28 11:03:34 -0400

    .

--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -6,19 +6,19 @@ import { Injectable } from '@angular/core';
 export class CustomerService {
 constructor(private http: HttpClient) { }
 
-  url="http://127.0.0.1:8080/ticket/add/";
+  url="http://127.0.0.1:8080/customer/add/";
 
 
-  createTicket(ticket:Ticket): Observable<any> {
+  createCustomer(customer:Customer): Observable<any> {
     let httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded'
       })
     }
-    let email = ticket.email;
-    let summary = ticket.summary;
-    let type = ticket.type;
-    let body = "email="+email+"&"+"summary="+summary+"&"+"type="+type;
+    let email = customer.email;
+    let name = customer.name;
+    let phone = customer.phone;
+    let body = "email="+email+"&"+"name="+name+"&"+"phone="+phone;
     return this.http.post(this.url, body, httpOptions);
   }
 }
