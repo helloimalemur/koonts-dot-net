b07cc89bf46f2a482fe63e6cbc87b4dce38f9ab5
James Koonts <jkoontsiii@gmail.com>
2022-09-07 15:15:12 -0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,23 +17,27 @@ export class AppComponent {
     //private overlayContainer:OverlayContainer
   ) {}
 
-  toggleMiniBar() {
-    if(this.minibar) {
-      this.minibar = false;
-    }
+  viewMiniBar() {
     if(!this.minibar) {
       this.minibar = true;
     }
   }
 
-  togglePortfolio() {
-    if(this.portfolio) {
+  closeMiniBar() {
+    if(this.minibar) {
+      this.minibar = false;
+    }
+  }
+
+  viewPortfolio() {
+    if(!this.portfolio) {
       this.portfolio = false;
     }
-    if(!this.portfolio) {
+  }
+  closePortfolio() {
+    if(this.portfolio) {
       this.portfolio = true;
     }
-
   }
 
 
@@ -45,34 +49,6 @@ export class AppComponent {
     let captionText = document.getElementById("caption");
     // @ts-ignore
     captionText.innerHTML = element.alt;
-  }
-
-  // Change style of navbar on scroll
-
-
-
-  myFunction() {
-    let navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      // @ts-ignore
-      navbar.className = "fx-bar" + " fx-card" + " fx-animate-top" + " fx-white";
-    } else {
-      // @ts-ignore
-      navbar.className = navbar.className.replace(" fx-card fx-animate-top fx-white", "");
-    }
-  }
-
-  // Used to toggle the menu on small screens when clicking on the menu button
-  toggleFunction() {
-    var x = document.getElementById("navDemo");
-    // @ts-ignore
-    if (x.className.indexOf("fx-show") == -1) {
-      // @ts-ignore
-      x.className += " fx-show";
-    } else {
-      // @ts-ignore
-      x.className = x.className.replace(" fx-show", "");
-    }
   }
 
 }
