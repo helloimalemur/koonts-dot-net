44687337354c14f8eaaaa09558d50376e649a4ef
James Koonts <jkoontsiii@gmail.com>
2022-09-08 14:18:47 -0400

    .

--- src/app/ticket.service.ts
+++ src/app/ticket.service.ts
@@ -13,7 +13,7 @@ export class TicketService {
 
   constructor(private http: HttpClient) { }
 
-  url="http://127.0.0.1:8080/ticket/add/";
+  url="http://127.0.0.1:8080/ticket/add";
 
 
   createTicket(ticket:Ticket): Observable<any> {
