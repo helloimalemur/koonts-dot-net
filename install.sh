cbf6e4583ff1bf24f4f03604cc82470e15798bc0
James Koonts <jkoontsiii@gmail.com>
2022-09-08 22:44:10 -0400

    discord notif working

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export NG_CLI_ANALYTICS=ci
+cd ~
+rm -rf koonts-dot-net/
+git clone https://github.com/helloimalemur/koonts-dot-net
+cd koonts-dot-net/
+ng config -g cli.warnings.versionMismatch false
+ng analytics off
+ng build
+npm install node
+ng build
+sudo rm -rf /srv/http/*
+sudo mv dist/koonts/* /srv/http/
+rm -rf koonts-dot-net/
