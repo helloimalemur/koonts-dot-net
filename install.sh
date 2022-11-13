f5d62981f5de4f9b3709a3d9fccede3b91c08977
James Koonts <jkoontsiii@gmail.com>
2022-11-13 16:24:06 -0500

    .

--- install.sh
+++ install.sh
@@ -6,8 +6,11 @@ git clone https://github.com/helloimalemur/koonts-dot-net
 cd koonts-dot-net/
 ng config -g cli.warnings.versionMismatch false
 export NG_CLI_ANALYTICS="false"
+echo "analytics off!.."
 ng analytics off
+echo "installing node!.."
 npm install node
+echo "attempting to build!.."
 ng build
 rm -rf ~/dist
 mkdir ~/dist
